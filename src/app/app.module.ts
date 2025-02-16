bccc8fc1c4c814cf8a4bef7f9f25960a620e4f4d
Pushpendra Sapkale <dev.psapkale@gmail.com>
2025-02-16 14:51:25 +0530

    Updated to 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,19 +9,12 @@ import { NgMaterialModule } from './ng-material/ng-material.module';
 import { ColorPickerModule } from 'ngx-color-picker';
 
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NgMaterialModule,
-    ColorPickerModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NgMaterialModule,
+        ColorPickerModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
